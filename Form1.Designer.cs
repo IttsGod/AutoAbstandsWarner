3a02c00fd4f6f01bbd50b967e42dc28afc0ebc2d
IttsGod <artremelp@gmail.com>
2023-06-20 21:40:35 +0200

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿
+namespace AutoAbstandsWarner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnInput = new System.Windows.Forms.Button();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.chbShAll = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(393, 158);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Hier Input eingeben";
+            // 
+            // btnInput
+            // 
+            this.btnInput.Location = new System.Drawing.Point(393, 238);
+            this.btnInput.Name = "btnInput";
+            this.btnInput.Size = new System.Drawing.Size(75, 23);
+            this.btnInput.TabIndex = 3;
+            this.btnInput.Text = "Input Speichern";
+            this.btnInput.UseVisualStyleBackColor = true;
+            this.btnInput.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(393, 194);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(100, 20);
+            this.txtInput.TabIndex = 4;
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(396, 90);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(75, 23);
+            this.btnCalc.TabIndex = 5;
+            this.btnCalc.Text = "Berechnen";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Location = new System.Drawing.Point(100, 127);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(0, 13);
+            this.lblOutput.TabIndex = 6;
+            // 
+            // chbShAll
+            // 
+            this.chbShAll.AutoSize = true;
+            this.chbShAll.Location = new System.Drawing.Point(396, 127);
+            this.chbShAll.Name = "chbShAll";
+            this.chbShAll.Size = new System.Drawing.Size(176, 17);
+            this.chbShAll.TabIndex = 7;
+            this.chbShAll.Text = "Nur Überschreitungen anzeigen";
+            this.chbShAll.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.chbShAll);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.btnInput);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnInput;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.Label lblOutput;
+        private System.Windows.Forms.CheckBox chbShAll;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+
+namespace AutoAbstandsWarner
+{
+    public partial class Form1 : Form
+    {
+        string file = "../../sensoren.txt";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            StreamWriter sw = File.AppendText(file);
+            sw.Write(txtInput.Text + Environment.NewLine);
+            sw.Close();
+            txtInput.Text = "";
+        }
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            StreamReader sr = File.OpenText(file);
+            String[] readLine = sr.ReadToEnd().Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
+            sr.Close();
+            lblOutput.Text = CalcOutput(readLine);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = "Ariks Auto Abstands Rechner";
+            this.AutoSize = true;
+            this.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+        }
+
+        private String CalcOutput(String[] input)
+        {
+            String SensorName = "";
+            String Output = "";
+            for (int i = 0; i <= input.Length - 2; i++)
+            {
+                String[] inputs = input[i].Split(';');
+                if (inputs[0] == "SL")
+                {
+                    SensorName = "Sensor Links";
+                }
+                else if (inputs[0] == "SM")
+                {
+                    SensorName = "Sensor Mitte";
+                }
+                else if (inputs[0] == "SR")
+                {
+                    SensorName = "Sensor Rechts";
+                }
+                String[] Date = inputs[1].Split('-');
+                String Datum = Date[2] + "." + Date[1] + "." + Date[0];
+                String Uhrzeit = inputs[2];
+                double Geschwindigkeit = Convert.ToDouble(inputs[3]);
+                double Abstand = Convert.ToDouble(inputs[4].Replace(".", ","));
+                String AbstandGenug = "Genügend Abstand gehalten";
+                if (Geschwindigkeit / 2.0 > Abstand)
+                {
+                    AbstandGenug = "Zu Wenig Abstand" +
+                        "\nBenötigter Abstand: " + Geschwindigkeit / 2 + " Meter" +
+                        "\nAktueller Abstand: " + Abstand + " Meter";
+                    if (chbShAll.Checked)
+                    {
+                        Output += "Messung " + (i + 1) + ": \nSensor: " + SensorName + "\n"
+                        + "Datum: " + Datum
+                        + "\nUhrzeit: " + Uhrzeit
+                        + "\nGeschwindigkeit: " + Geschwindigkeit + " km/h"
+                        + "\nAbstand: " + Abstand + " Meter"
+                        + "\n" + AbstandGenug + "\n\n";
+                    }
+                }
+                if (!chbShAll.Checked)
+                {
+                    Output += "Messung " + (i + 1) + ": \nSensor: " + SensorName + "\n"
+                    + "Datum: " + Datum
+                    + "\nUhrzeit: " + Uhrzeit
+                    + "\nGeschwindigkeit: " + Geschwindigkeit + " km/h"
+                    + "\nAbstand: " + Abstand + " Meter"
+                    + "\n" + AbstandGenug + "\n\n";
+                }
+            }
+            return Output;
+        }
+    }
+}
